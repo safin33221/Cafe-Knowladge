c8aa1b9d8ffa3f332422c6ac6577a9e1083126f0
safin33221 <saifn33221@gmail.com>
2024-10-21 22:03:10 +0600

    initialize setup dove

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className="text-2xl font-bold text-purple-600">Cafe</h1>
+        </div>
+    );
+};
+
+export default Header;
